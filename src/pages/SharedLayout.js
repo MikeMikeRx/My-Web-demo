443b94532d7ee144c1f590405915fb82129f9ed8
MichalRais <Michal.Rais@iCloud.com>
2025-07-29 10:19:14 +0100

    Import Navbar and Footer to SharedLayout.js

---
+++ src/pages/SharedLayout.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+const SharedLayout = () =>{
+    return <>
+        <Navbar />
+    
+        <Footer />
+    </>
+}

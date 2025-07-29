9454c09a9d89006a44493f7b726bd70fb28df2f2
MichalRais <Michal.Rais@iCloud.com>
2025-07-29 15:04:37 +0100

    Add inputs to Form.js

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,7 +1,28 @@
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
+  return <section>
+    <form>
+      <input 
+      type="text" 
+      placeholder="Title"
+      />
+
+      <input type="number"
+      placeholder="Minimal age"
+      min="1"       
+      />
+
+      <input type="number"
+      placeholder="Movie duration"
+      min="1"
+      />
+
+      <input type="submit"
+      value="Add movie" 
+      />
+      
+    </form>
+
+  </section>
 }
 
 export default Form

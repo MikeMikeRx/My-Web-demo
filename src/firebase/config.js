20d97320a07da9a96b44a77323811505847018b3
MichalRais <Michal.Rais@iCloud.com>
2025-07-29 12:10:39 +0100

    Implement Firestore data fetch for AllMovies component

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,12 @@
-import firebase from "./firebase/app"
+import firebase from "firebase/app"
 import "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyDrGiHSJRa-RX4FzR3QLuR7eD4DJAsaQLg",
   authDomain: "movies-project-2-bbc16.firebaseapp.com",
   projectId: "movies-project-2-bbc16",
-  storageBucket: "movies-project-2-bbc16.firebasestorage.app",
+  // storageBucket: "movies-project-2-bbc16.firebasestorage.app",
+  storageBucket: "movies-project-2-bbc16.appspot.com",
   messagingSenderId: "83834647438",
   appId: "1:83834647438:web:55e775d2ff7c4540076adf"
 };
@@ -14,4 +15,4 @@ firebase.initializeApp(firebaseConfig)
 
 const projectFirestore = firebase.firestore()
 
-export { projectFirestore }
+export { projectFirestore } 
--- src/pages/AllMovies.js
+++ src/pages/AllMovies.js
@@ -1,7 +1,33 @@
+import { projectFirestore } from "../firebase/config"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+
 const AllMovies = () => {
-  return (
-    <div>AllMovies</div>
-  )
+  const [data, setData] = useState([])
+  const [error, setError] = useState("")
+
+  useEffect( () =>{
+    projectFirestore.collection("movies").get().then( (snapshot) =>{
+      console.log(snapshot.docs)
+
+      if (snapshot.empty){
+        setError("No movies found")
+      } else {
+        let result = []
+        snapshot.docs.forEach( (oneMovie) => {                              
+          result.push( {id: oneMovie.id, ...oneMovie.data()} )
+        })
+        setData(result)        
+      }
+      
+    })
+  },[])
+
+
+
+  return <section>
+    {error && <p>{error}</p>}
+  </section>
 }
 
 export default AllMovies

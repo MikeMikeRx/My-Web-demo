05e187b4127b8ae564a8d369bbccda47fb661d99
MichalRais <Michal.Rais@iCloud.com>
2025-07-29 10:07:54 +0100

    Create and import 3 pages

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import AllMovies from "./pages/AllMovies"
+import OneMovie from "./pages/OneMovie"
+
+const App = () => {
+  return <BrowserRouter>
+  <Routes>
+
+    <Route path="/" element={ <Home/> } />
+    <Route path="allmovies" element={ <AllMovies/> } />
+    <Route path="one-movie" element={ <OneMovie/> } />
+
+  </Routes>  
+</BrowserRouter>
+}
+
+export default App

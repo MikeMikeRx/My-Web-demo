673b18a8689673566f7b6368dc9b6614f8dbb237
MichalRais <Michal.Rais@iCloud.com>
2025-07-29 15:13:49 +0100

    Import useState and Add submitForm function

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,25 +1,44 @@
+import { useState } from "react"
+import { projectFirestore } from "../firebase/config"
+
 const Form = () => {
+  const [movieTitle, setMovieTitle] = useState("")
+  const [movieAge, setMovieAge] = useState(null)
+  const [movieTime, setMovieTime] = useState(null)
+
+  const submitForm = (e) => {
+    e.preventDefault()
+
+    console.log(movieTitle)
+    console.log(movieAge)
+    console.log(movieTime)
+
+  }
+
   return <section>
-    <form>
+    <form onSubmit={submitForm}>
       <input 
       type="text" 
       placeholder="Title"
+      onChange={ (e) => setMovieTitle(e.target.value) }
       />
 
       <input type="number"
       placeholder="Minimal age"
-      min="1"       
+      min="1"
+      onChange={ (e) => setMovieAge(e.target.value) }       
       />
 
       <input type="number"
       placeholder="Movie duration"
+      onChange={ (e) => setMovieTime(e.target.value) }
       min="1"
       />
 
       <input type="submit"
       value="Add movie" 
       />
-      
+
     </form>
 
   </section>
